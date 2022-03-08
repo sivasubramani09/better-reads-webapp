8a9cfeb64df558268dd5caf2ee4235588179bced
sivasubramani09 <36350531+sivasubramani09@users.noreply.github.com>
2022-03-09 01:51:12 +0530

    Add Book page and Controller

---
+++ src/test/java/com/betterreads/BetterReadsApp/BetterReadsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betterreads.BetterReadsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReadsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
